491c13f80a4b0cbc8d90621bd622554a0bf8b3ac
KennethM19 <ken.jav550@gmail.com>
2025-05-31 00:56:22 -0500

    'dataUser'

--- users/models.py
+++ users/models.py
@@ -1,31 +1,67 @@
 from django.core.validators import RegexValidator
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .managers import CustomUserManager
 
 
 # Create your models here.
 class Roles(models.Model):
     role = models.CharField(max_length=100)
 
-class User(models.Model):
+    def __str__(self):
+        return self.role
+
+class User(AbstractBaseUser, PermissionsMixin):
     doc_number = models.CharField(max_length=100, unique=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    phone = models.CharField(max_length=15,
-                             blank=True,
-                             null=True,
-                             validators=[RegexValidator(regex=r'^\+?1?\d{9,15}$',
-                                                        message="Formato inválido para el número de teléfono.")],
-                             verbose_name="Phone"
-                             )
+    phone = models.CharField(
+        max_length=15,
+        blank=True,
+        null=True,
+        validators=[RegexValidator(regex=r'^\+?1?\d{9,15}$',
+                                   message="Formato inválido para el número de teléfono.")],
+        verbose_name="Phone"
+    )
     email = models.EmailField(max_length=100, unique=True)
-    password = models.CharField(max_length=100)
-    role = models.ForeignKey(Roles, on_delete=models.CASCADE)
+    password = models.CharField(max_length=128)
+    role = models.ForeignKey('Roles', on_delete=models.CASCADE, default=1)
     date_created = models.DateTimeField(auto_now_add=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'  # Campo usado para login
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'doc_number']
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+        return f"{self.first_name} {self.last_name}"
 
 class Card(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     number = models.CharField(max_length=100)
-    cvv = models.CharField(max_length=100)
     date_expired = models.DateTimeField(null=True)
     owner = models.CharField(max_length=100)
     is_credit = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if len(self.number) > 4:
+            self.number = self.number[-4:]
+        super().save(*args, **kwargs)
+
+class Address(models.Model):
+    user = models.OneToOneField("User", on_delete=models.CASCADE)
+
+    street = models.CharField(max_length=100)
+    number = models.CharField(max_length=10)
+    apartment = models.CharField(max_length=10, blank=True, null=True)
+    district = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    region = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=20, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.street}, {self.city}"
--- users/views.py
+++ users/views.py
@@ -1,17 +1,16 @@
 # Create your views here.
-from .models import User
+from .models import User, Card
 from django.contrib.auth.hashers import check_password
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework_simplejwt.views import TokenObtainPairView
-from .serializers import UserSerializer
-
+from .serializers import UserCreateSerializer, UserSerializer, CardSerializer, CardCreateSerializer
 
 @api_view(['POST'])
 def register(request):
-    serializer = UserSerializer(data=request.data)
+    serializer = UserCreateSerializer(data=request.data)
     if serializer.is_valid():
         user = serializer.save()
         refresh = RefreshToken.for_user(user)
@@ -31,25 +30,49 @@ def login(request):
     try:
         user = User.objects.get(email=email)
     except User.DoesNotExist:
-        return Response({'error':'Usuario no encontrado'},status=status.HTTP_404_NOT_FOUND)
+        return Response({'error': 'Usuario no encontrado'}, status=status.HTTP_404_NOT_FOUND)
+
     if not check_password(password, user.password):
-        return Response({'error':'Contraseña incorrecta'}, status=status.HTTP_403_FORBIDDEN)
+        return Response({'error': 'Contraseña incorrecta'}, status=status.HTTP_403_FORBIDDEN)
 
     refresh = RefreshToken.for_user(user)
 
     return Response({
         'refresh': str(refresh),
         'access': str(refresh.access_token),
-        'user': {
-            'id': user.id,
-            'email': user.email,
-            'first_name': user.first_name,
-            'last_name': user.last_name,
-            'role': user.role.role
-        }
-    },status=status.HTTP_200_OK)
+    }, status=status.HTTP_200_OK)
 
 @api_view(['GET'])
-def users(request):
-    serializer = UserSerializer(User.objects.all(), many=True)
+@permission_classes([IsAuthenticated])
+def get_user(request):
+    serializer = UserSerializer(request.user)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_card(request):
+    cards = Card.objects.filter(user=request.user)
+    serializer = CardSerializer(cards, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def register_card(request):
+    serializer = CardCreateSerializer(data=request.data)
+    if serializer.is_valid():
+        validated_data = serializer.validated_data
+
+        last_four = validated_data['number'][-4:]
+        masked_number = f"**** **** **** {last_four}"
+
+        card = Card.objects.create(
+            number=masked_number,
+            date_expired=validated_data['date_expired'],
+            owner=validated_data['owner'],
+            is_credit=validated_data['is_credit'],
+            user=request.user
+        )
+        return Response({"message": "Tarjeta registrada correctamente."}, status=status.HTTP_201_CREATED)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
